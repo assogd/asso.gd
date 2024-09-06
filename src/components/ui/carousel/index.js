18b8d7709a3feb28f946a6bff1b51ba185e24895
mathiasdag <mathias@asso.gd>
2024-09-06 15:45:25 +0200

    ...

--- src/components/ui/carousel/index.js
+++ src/components/ui/carousel/index.js
@@ -209,7 +209,7 @@ export const MainCarousel = ({ content }) => {
         ))}
       </div>
 
-      <nav className={clsx('fixed right-0 top-0', 'select-none')}>
+      <nav className={clsx('fixed right-0 top-0 z-10', 'select-none')}>
         <Link href="/about" className={'block p-4'}>
           About the Studio
         </Link>
