91ea5a8ed62f7092b0d87890f80a753aeb1d743a
mathiasdag <mathias@asso.gd>
2024-09-17 16:51:10 +0200

    ...

--- src/components/ui/carousel/index.js
+++ src/components/ui/carousel/index.js
@@ -16,7 +16,7 @@ export const MainCarousel = ({ content }) => {
   const [isPaused, setIsPaused] = useState(false)
   const [progress, setProgress] = useState(0)
   const [wasHolding, setWasHolding] = useState(false) // Tracks if it was holding to pause
-  const [isLocked, setIsLocked] = useState(false) // To prevent multiple navigation events
+  const [isLocked, setIsLocked] = useState(false) // To prevent multiple navigation events or hold-triggered navigation
   const router = useRouter()
   const intervalRef = useRef(null)
   const startTimeRef = useRef(null)
@@ -108,6 +108,7 @@ export const MainCarousel = ({ content }) => {
     isHoldingRef.current = false // Reset hold flag
     isSwipingRef.current = false // Reset swipe flag
     setWasHolding(false) // Reset the was holding state
+    setIsLocked(true) // Lock navigation during hold
     pauseTimer()
   }
 
@@ -121,7 +122,12 @@ export const MainCarousel = ({ content }) => {
       setWasHolding(true)
     }
 
-    if (!isHoldingRef.current && !isSwipingRef.current && !wasHolding) {
+    if (
+      !isHoldingRef.current &&
+      !isSwipingRef.current &&
+      !wasHolding &&
+      !isLocked
+    ) {
       // Only navigate if it wasn't a long hold or a swipe or holding pause
       if (clickX < screenWidth / 2) {
         handlePrev()
@@ -130,6 +136,7 @@ export const MainCarousel = ({ content }) => {
       }
     }
 
+    setIsLocked(false) // Unlock navigation after the hold
     isHoldingRef.current = false
     resumeTimer()
   }
@@ -143,6 +150,7 @@ export const MainCarousel = ({ content }) => {
     isHoldingRef.current = false // Reset hold flag
     isSwipingRef.current = false // Reset swipe flag
     setWasHolding(false) // Reset the was holding state
+    setIsLocked(true) // Lock navigation during hold
     pauseTimer()
     const startTouchX = e.touches[0].clientX
     e.target.dataset.startTouchX = startTouchX // Store touch start point for swipe detection
@@ -168,14 +176,19 @@ export const MainCarousel = ({ content }) => {
       setWasHolding(true)
     }
 
-    if (!isHoldingRef.current && !isSwipingRef.current && !wasHolding) {
+    if (
+      !isHoldingRef.current &&
+      !isSwipingRef.current &&
+      !wasHolding &&
+      !isLocked
+    ) {
       // Only navigate if it wasn't a long hold or a swipe or holding pause
       if (touchX < screenWidth / 2) {
         handlePrev()
       } else {
         handleNext()
       }
-    } else if (isSwipingRef.current && !wasHolding) {
+    } else if (isSwipingRef.current && !wasHolding && !isLocked) {
       if (touchX > startTouchX) {
         handlePrev() // Swipe right
       } else {
@@ -183,6 +196,7 @@ export const MainCarousel = ({ content }) => {
       }
     }
 
+    setIsLocked(false) // Unlock navigation after the hold
     isHoldingRef.current = false
     resumeTimer()
   }
