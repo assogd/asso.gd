38af4445be691d2ea37282dfec11701e263155b3
mathiasdag <mathias@asso.gd>
2024-09-17 17:09:29 +0200

    ...

--- src/components/ui/navigation.js
+++ src/components/ui/navigation.js
@@ -27,7 +27,7 @@ export const Navigation = () => {
             </Link>
           </motion.div>
           <nav className={clsx('fixed right-0 top-0 z-10', 'select-none')}>
-            <Link href="/about" className={'block px-4 py-3'}>
+            <Link href="/about" className={'block p-4'}>
               About the Studio
             </Link>
           </nav>
