767fa0d7ee4885112bc9b4897ea3555773ced31b
mathiasdag <mathias@asso.gd>
2025-01-09 14:24:42 +0100

    nicer not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,12 +3,7 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <main className="p-2 pb-8 text-center flex flex-col gap-8 justify-center items-center fixed inset-0">
-      <nav className={'fixed right-0 top-0 select-none'}>
-        <Link href="/about" className={'block p-4'}>
-          About the Studio
-        </Link>
-      </nav>
+    <main className="text-center h-[calc(100vh-6.5em)] pb-8 flex items-center justify-center">
       <h1>Page not found</h1>
     </main>
   )
